8f4fe150ae2ce32c669dbadcadd9b8e245c788f9
Michael  WHITE <michael.white@pasteur.fr>
2018-11-27 15:22:19 +0100

    Continued fixeing of = signs

--- Pv_mod/Source.cpp
+++ Pv_mod/Source.cpp
@@ -3073,7 +3073,7 @@ void intervention_dist(double t, params* theta, population* POP, intervention* I
 					/////////////////////////////////////////////////////////////////////
 					// Was there PQ overtreatment?
 
-					if ((PQ_treat == 1) && (POP->people[n].Hyp = 0))
+					if ((PQ_treat == 1) && (POP->people[n].Hyp == 0))
 					{
 						POP->people[n].PQ_overtreat = 1;
 					}
@@ -3238,7 +3238,7 @@ void intervention_dist(double t, params* theta, population* POP, intervention* I
 					/////////////////////////////////////////////////////////////////////
 					// Was there PQ overtreatment?
 
-					if ((PQ_treat == 1) && (POP->people[n].Hyp = 0))
+					if ((PQ_treat == 1) && (POP->people[n].Hyp == 0))
 					{
 						POP->people[n].PQ_overtreat = 1;
 					}
