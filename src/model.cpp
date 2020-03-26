3da65dd3b0331d1c18c8561acf9308701211ad58
Giovanni Charles <giovanni.charles@gmail.com>
2020-03-26 17:02:46 +0000

    Fix comment encoding error

--- src/model.cpp
+++ src/model.cpp
@@ -2824,7 +2824,7 @@ double gammln(const double xx)
 //  Given a matrix a[1..n][1..n], this routine replaces it by the LU decomposition of a rowwise       //
 //  permutation of itself. a and n are input. a is output, arranged as in equation (2.3.14) above;    //
 //  indx[1..n] is an output vector that records the row permutation effected by the partial           //
-//  pivoting; d is output as �1 depending on whether the number of row interchanges was even          //
+//  pivoting; d is output as ±1 depending on whether the number of row interchanges was even          //
 //  or odd, respectively. This routine is used in combination with lubksb to solve linear equations   //
 //  or invert a matrix                                                                                //
 //                                                                                                    // 
