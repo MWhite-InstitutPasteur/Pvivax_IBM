5582aad8b5f92657b7959572c806939f0f8af09d
Giovanni Charles <giovanni.charles@gmail.com>
2020-03-27 10:58:34 +0000

    Replace manual approximate assertion with catch

--- src/test-rng.cpp
+++ src/test-rng.cpp
@@ -19,37 +19,33 @@ void set_seed(double seed) {
     set_seed_r(std::floor(std::fabs(seed)));
 }
 
-bool almost_equal(double a, double b, double delta) {
-	return a < b + delta && a > b - delta;
-}
-
 context("Random number generation") {
-  test_that("Exponential generation is reproducible") {
-	  set_seed(42);
-	  expect_true(almost_equal(genexp(1), 0.1983368, .0001));
-  }
+    test_that("Exponential generation is reproducible") {
+        set_seed(42);
+        CATCH_REQUIRE(genexp(1) == Approx(0.1983368));
+    }
 
-  test_that("Normal generation is reproducible") {
-  	  set_seed(42);
-  	  expect_true(almost_equal(gennor(0, 1), 1.370958, .0001));
-  }
+    test_that("Normal generation is reproducible") {
+        set_seed(42);
+        CATCH_REQUIRE(gennor(0, 1) == Approx(1.370958));
+    }
 
-  test_that("Uniform generation is reproducible") {
-	  set_seed(42);
-	  expect_true(almost_equal(genunf(0, 1), 0.914806, .0001));
-  }
+    test_that("Uniform generation is reproducible") {
+        set_seed(42);
+        CATCH_REQUIRE(genunf(0, 1) == Approx(0.914806));
+    }
 
-  test_that("Multivariate normal generation is reproducible") {
-  	  set_seed(42);
-  	  float covm[] = {1., 0, 0, 1.};
-  	  float meanv[] = {0., 0.};
-  	  long p = 2L;
-  	  float param[10];
-  	  setgmn(meanv, covm, p, param);
-  	  float result[2];
-  	  float work[2];
-  	  genmn(param, result, work);
-  	  expect_true(almost_equal(result[0], 1.37096, .0001));
-  	  expect_true(almost_equal(result[1], -0.564698, .0001));
-  }
+    test_that("Multivariate normal generation is reproducible") {
+        set_seed(42);
+        float covm[] = {1., 0, 0, 1.};
+        float meanv[] = {0., 0.};
+        long p = 2L;
+        float param[10];
+        setgmn(meanv, covm, p, param);
+        float result[2];
+        float work[2];
+        genmn(param, result, work);
+        CATCH_REQUIRE(result[0] == Approx(1.37096));
+        CATCH_REQUIRE(result[1] == Approx(-0.564698));
+    }
 }
